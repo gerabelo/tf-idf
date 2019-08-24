5e412a84ce5641a38b4d1e9a9c3971c90322eb8c
unknown <=>
2019-08-23 23:09:46 -0400

    23082019214922

--- dict_tf.py
+++ dict_tf.py
@@ -10,18 +10,20 @@ ts = time.time()
 
 client = MongoClient("mongodb://localhost:27017")
 db_src = client['facebook']
-db_dst = client['dicionario']
+db_dst = client['dicionario3']
 # collection_src = db_src['engenharia.da.computacao.ufam']
 # collection_dst = db_dst['engenharia.da.computacao.ufam']
 # collection_dst.create_index([('term',TEXT)],name='search_index', default_language='portuguese')
 
-sources = ['ACriticaCom','UOLNoticias','bncplay','classificadosam','classificadosmanaus','compraevendamanaus','g1','radiocbn']
+sources = ['ACriticaCom']#,'UOLNoticias','bncplay','classificadosam','classificadosmanaus','compraevendamanaus','g1','radiocbn']
+
+data = [[],[]]
 
 for source in sources:
     collection_src = db_src[source]
     collection_dst = db_dst[source]
     collection_dst.create_index([('term',TEXT)],name='search_index', default_language='portuguese')
-    
+
     artigos = collection_src.find({})
     terms_counter = 0
     for artigo in artigos:
@@ -36,11 +38,26 @@ for source in sources:
 
             terms = quote.split()
             for term in terms:
-                if collection_dst.find({"term":term}).count() > 0:
-                    collection_dst.update_one({"term":term},{"$inc":{"tf":1}})
-                else:
-                    terms_counter += 1
-                    collection_dst.insert_one({"term":term, "tf":1})
+                # if collection_dst.find({"term":term}).count() > 0:
+                #     collection_dst.update_one({"term":term},{"$inc":{"tf":1}})
+                # else:
+                #     terms_counter += 1
+                #     collection_dst.insert_one({"term":term, "tf":1})
+                i = len(data[0])
+                j = 0
+                while i > 0:
+                    i -= 1
+                    if data[0][i] == term:
+                        data[1][i] += 1
+                        j = 1
+                if j == 0:
+                    data[0].append(term)
+                    data[1].append(1)
+i = len(data[0])
+while i > 0:
+    i -= 1
+    print(data[0][i],data[1][i])
+    collection_dst.insert_one({"term":data[0][i],"tf":data[1][i]})
 
 print("total de termos: ",terms_counter)
 print("finalizado em ",time.time()-ts," segundos")
